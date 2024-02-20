66e6fc1dc51ef84a28b4afbfc8c37cf2bbde13c0
MC <chokamoses2@gmail.com>
2024-02-20 18:05:49 +0300

    Added index on users email, reinforced uniqueness

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,5 +55,12 @@ class UserTest < ActiveSupport::TestCase
 
   end
 
+  # TEST FOR A UNIQUE EMAIL ADDRESS
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 
 end
