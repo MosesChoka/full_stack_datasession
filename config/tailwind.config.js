89ea7c565a40e63d8ffdf8bcc10e62007c651aae
MC <chokamoses2@gmail.com>
2024-06-10 22:33:49 +0300

    updated course section on landing page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+	    colors: {
+		    'custom-btn-bg': '#098087',
+		    'custom-hover' : 'rgb(235, 55, 55)',
+		    'courses-bg' : 'rgba(255, 228, 196, 0.1)',
+        'about-bg': 'rgb(240, 248, 255)',
+	    },
+
+	    width: {
+		    '550':'550px',
+		    '350':'350px',
+		    '300':'300px',
+		    '100':'100px',
+	    },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
